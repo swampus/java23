6bb4be82dfe31f7b537faf7f3476949b2e95d65c
Dmitry <dmitrijs.gavrilovs.swampus@gmail.com>
2023-03-08 20:24:34 +0200

    bmw button

--- src/main/java/org/example/second/controller/FXMLExampleController.java
+++ src/main/java/org/example/second/controller/FXMLExampleController.java
@@ -2,12 +2,42 @@ package org.example.second.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+import org.example.second.model.Car;
+import org.example.second.model.DatabaseMock;
+
+import java.util.List;
 
 public class FXMLExampleController {
 
     @FXML
     private Button bwmSearch;
 
+    @FXML
+    private Button audiSearch;
+
+    @FXML
+    private ListView<Text> dataGrid;
+
+    @FXML
+    public void initialize() {
+        DatabaseMock databaseMock = new DatabaseMock();
+
+        bwmSearch.setOnMouseClicked(t -> {
+            List<Car> bmws = databaseMock.getBwm();
+            Text text = new Text();
+            for (Car bmw : bmws) {
+                text.setText(
+                        text.getText() + " "
+                                + bmw.getId() + " "
+                                + bmw.getType() + " "
+                                + bmw.getPrice());
+            }
+            dataGrid.getItems().clear();
+            dataGrid.getItems().add(text);
+        });
+    }
 
 
 }
